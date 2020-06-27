f23c868e58043826eb68b748fe389e59675bbdc4
leeyunhome <leeyunhome@daum.net>
2020-06-27 13:19:47 +0900

    add lower and uppercase both

--- HelloWorld/Cryptography/Cryptography.c
+++ HelloWorld/Cryptography/Cryptography.c
@@ -7,6 +7,7 @@
 int main()
 {
 	char input_string[MAX_STR];
+	char encrypted_string[MAX_STR];
 
 	scanf("%s", input_string);
 	
@@ -14,16 +15,43 @@ int main()
 	{
 		char c = input_string[i];
 
+		encrypted_string[i] = c;
+
 		if (c == '\0') break;
 
 		int ascii = (int)c;	// casting char to int
 		int ascii_en = ascii - 3; // Caesar cipher
 
-		if (ascii_en < 65) ascii_en += 26;
+		if ((ascii_en > 90 && ascii_en < 97) || ascii_en < 65)
+		{
+			ascii_en += 26;
+		}
 
 		char c_en = (char)ascii_en;
 
-		printf("%c ", c_en);
+		encrypted_string[i] = c_en;
+		//printf("%c ", c_en);
+	}
+
+	printf("%s\n", encrypted_string);
+
+	for (int i = 0; i < MAX_STR; i++)
+	{
+		char c = encrypted_string[i];
+
+		if (c == '\0') break;
+
+		int ascii = (int)c;// casting char to int
+		int ascii_de = ascii + 3;	// Caesar cipher
+
+		if ((ascii_de > 90 && ascii_de < 97) || ascii_de > 122)
+		{
+			ascii_de -= 26;
+		}
+
+		char c_de = (char)ascii_de;
+
+		printf("%c", c_de);
 	}
 
 	return 0;
