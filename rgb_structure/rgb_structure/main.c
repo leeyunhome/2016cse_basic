84c6103ad8ed8fb330011c43b4da3d732ebca746
leeyunhome <leeyunhome@daum.net>
2020-06-28 13:26:39 +0900

    add reapeating boundary

--- rgb_structure/rgb_structure/main.c
+++ rgb_structure/rgb_structure/main.c
@@ -5,7 +5,24 @@
 #include "BMPImage.h"
 #include <stdbool.h>
 #include <assert.h>
- 
+
+RGB getPixelColor(const RGB* rgb_array, const int res_x, const int res_y, int i, int j)
+{
+    // repeating boundary
+    if (i < 0) i += res_x;
+    if (j < 0) j += res_y;
+
+    i = i % res_x;
+    j = j % res_y;
+
+    const int ix = i + res_x * j;
+
+    assert(ix >= 0);
+    assert(ix < res_x* res_y);
+
+    return rgb_array[ix];
+}
+
 int main()
 {
     int res_x, res_y;
@@ -15,7 +32,18 @@ int main()
 
     readBMP24("sample.bmp", &res_x, &res_y, &rgb_array);
 
-    writeBMP24("output.bmp", res_x, res_y, rgb_array);
+    RGB* rgb_temp = (RGB*)malloc(sizeof(RGB) * res_x * res_y);
+    
+    for (int j = 0; j < res_y; j++)
+    {
+        for (int i = 0; i < res_x; i++)
+        {
+            rgb_temp[i + res_x * j] = getPixelColor(rgb_array, res_x, res_y, i + res_x/2, j + res_y/2);
+        }
+    }
+    writeBMP24("output1.bmp", res_x, res_y, rgb_temp);
 
+    free(rgb_array);
+    free(rgb_temp);
     return 0;
 }
